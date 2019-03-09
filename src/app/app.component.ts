a6b7ed9d2eb01f97863215ab9d9488f4f7ea6b92
Matthew Toll <matttoll@masterpeaceltd.com>
2019-03-09 16:37:54 -0500

    initial demo working just to prove out the setup necessary for JointJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as jQuery from 'jquery';
+import * as _ from 'lodash';
+import * as $ from 'backbone';
+import * as joint from 'jointjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jointjs-demo';
+
+
+  ngOnInit() {
+    let graph = new joint.dia.Graph;
+
+    let paper = new joint.dia.Paper({
+      el: jQuery("#paper"),
+      width: 600,
+      height: 200,
+      model: graph,
+      gridSize: 1
+    });
+
+    let rect = new joint.shapes.basic.Rect({
+      position: { x: 100, y: 30 },
+      size: { width: 100, height: 30 },
+      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+    let rect2 = rect.clone() as joint.shapes.basic.Rect;
+    rect2.translate(300);
+
+    var link = new joint.dia.Link({
+      source: { id: rect.id },
+      target: { id: rect2.id }
+    });
+
+    graph.addCells([rect, rect2, link]);
+  }
+
 }
